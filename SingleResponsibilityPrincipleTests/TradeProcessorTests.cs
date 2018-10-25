c9cf502a504b616eabae65da01d0a053d9c63a93
Dustin Kowal <dkowal@css.edu>
2018-10-25 11:41:37 -0500

    Attempted to start unit testing

---
+++ SingleResponsibilityPrincipleTests/TradeProcessorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SingleResponsibilityPrinciple;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.Tests
+{
+    [TestClass()]
+    public class TradeProcessorTests
+    {
+        private int CountDbRecords()
+        {
+            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dkowal\source\repos\cis-3285-asg-8-dustinkowal\tradesdatabase.mdf;Integrated Security=True;Connect Timeout=30"))
+            {
+                connection.Open();
+                string myScalarQuery = "SELECT COUNT(*) FROM trade";
+                SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
+                myCommand.Connection.Open();
+                int count = (int)myCommand.ExecuteScalar();
+                connection.Close();
+                return count;
+            }
+        }
+        [TestMethod()]
+        public void TestNormalFile()
+            //modify so it calls count before and after process trades
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.goodtrades.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Act
+            int countB = CountDbRecords();
+            tradeProcessor.ProcessTrades(tradeStream);
+
+            //Assert
+            int countA = CountDbRecords();
+            Assert.AreEqual(countB + 4, countA);
+        }
+    }
+}
